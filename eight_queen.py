59aa770e6890ab182aff9ddf133a8b00486a07d6
NSatoh-azb <nsatoh.azb@gmail.com>
2016-01-22 21:12:01 +0900

    配置の判定を行う関数admissibleの中身を記述．これはひどい．
    一応動くようになったハズ．

--- eight_queen.py
+++ eight_queen.py
@@ -7,7 +7,30 @@ def admissible(i1,i2,i3,i4,i5,i6,i7,i8):
     同じ列にないことの確認と，同じ対角線上にないことの確認をして，
     OKならTrue，ダメならFalseを返す
     """
-    pass
+    return (
+        (i1 != i2) and (i1 != i3) and (i1 != i4) and (i1 != i5)
+    and (i1 != i6) and (i1 != i7) and (i1 != i8)
+    and (i2 != i3) and (i2 != i4) and (i2 != i5) and (i2 != i6)
+    and (i2 != i7) and (i2 != i8)
+    and (i3 != i4) and (i3 != i5) and (i3 != i6) and (i3 != i7)
+    and (i3 != i8)
+    and (i4 != i5) and (i4 != i6) and (i4 != i7) and (i4 != i8)
+    and (i5 != i6) and (i5 != i7) and (i5 != i8)
+    and (i6 != i7) and (i6 != i8)
+    and (i7 != i8)
+    and abs(i1 - i2) != 1 and abs(i1 - i3) != 2 and abs(i1 - i4) != 3
+    and abs(i1 - i5) != 4 and abs(i1 - i6) != 5 and abs(i1 - i7) != 6
+    and abs(i1 - i8) != 7
+    and abs(i2 - i3) != 1 and abs(i2 - i4) != 2 and abs(i2 - i5) != 3
+    and abs(i2 - i6) != 4 and abs(i2 - i7) != 5 and abs(i2 - i8) != 6
+    and abs(i3 - i4) != 1 and abs(i3 - i5) != 2 and abs(i3 - i6) != 3
+    and abs(i3 - i7) != 4 and abs(i3 - i8) != 5
+    and abs(i4 - i5) != 1 and abs(i4 - i6) != 2 and abs(i4 - i7) != 3
+    and abs(i4 - i8) != 4
+    and abs(i5 - i6) != 1 and abs(i5 - i7) != 2 and abs(i5 - i8) != 3
+    and abs(i6 - i7) != 1 and abs(i6 - i8) != 2
+    and abs(i7 - i8) != 1
+    )
 
 
 # メインルーチン
