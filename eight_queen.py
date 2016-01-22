0992c721081b391abd2d91ae22a228e16da827d1
NSatoh-azb <nsatoh.azb@gmail.com>
2016-01-22 21:04:53 +0900

    下手版の作成を開始．とりあえず探索のためのメインルーチンと判定用の関数を準備．

---
+++ eight_queen.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+def admissible(i1,i2,i3,i4,i5,i6,i7,i8):
+    """
+    i1 ～ i8 のすべてについて，
+    同じ列にないことの確認と，同じ対角線上にないことの確認をして，
+    OKならTrue，ダメならFalseを返す
+    """
+    pass
+
+
+# メインルーチン
+for i1 in range(8):
+    for i2 in range(8):
+        for i3 in range(8):
+            for i4 in range(8):
+                for i5 in range(8):
+                    for i6 in range(8):
+                        for i7 in range(8):
+                            for i8 in range(8):
+                                if admissible(i1,i2,i3,i4,i5,i6,i7,i8):
+                                    print(i1,i2,i3,i4,i5,i6,i7,i8)
